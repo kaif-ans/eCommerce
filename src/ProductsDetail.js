76eaa9ee24cbf9a7380618f1f86c0a6ef7afac5d
kaif-ans <kaifansari989@gmail.com>
2022-10-26 11:29:46 +0530

    rechecked

--- src/ProductsDetail.js
+++ src/ProductsDetail.js
@@ -5,7 +5,6 @@ import { useParams } from "react-router-dom";
 export default function ProductsDetail() {
   const { productId } = useParams();
   const thisProduct = productsData.find((prod) => prod.id == productId);
-  console.log("gitcheck")
   return (
     <div>
       <div className="detail-box">
